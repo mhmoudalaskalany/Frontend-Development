f31a291a6064721cc54ac7a15c7f885e11634a76
Mahmoud Alaskalany <mhmoudalaskalany@gmail.com>
2025-09-10 13:27:56 +0400

    add signals component

--- Angular/angular-tutorials/src/app/app.routes.ts
+++ Angular/angular-tutorials/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Signals } from './signals/signals/signals';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'signals',
+    component: Signals
+}];
---
+++ Angular/angular-tutorials/src/app/signals/signals/signals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  imports: [],
+  templateUrl: './signals.html',
+  styleUrl: './signals.css'
+})
+export class Signals {
+
+}
