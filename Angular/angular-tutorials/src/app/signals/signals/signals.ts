b60416f95d4348e3fd4b076d03a395fc2e308c73
Mahmoud Alaskalany <mhmoudalaskalany@gmail.com>
2025-09-11 12:16:55 +0400

    add effects outside of constructor

--- Angular/angular-tutorials/src/app/signals/signals/signals.ts
+++ Angular/angular-tutorials/src/app/signals/signals/signals.ts
@@ -1,4 +1,15 @@
-import { Component, computed, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  OnInit,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChildSignal } from '../components/child-signal/child-signal';
 
@@ -7,12 +18,17 @@ import { ChildSignal } from '../components/child-signal/child-signal';
   imports: [FormsModule, ChildSignal],
   templateUrl: './signals.html',
   styleUrl: './signals.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Signals implements OnInit {
   count: WritableSignal<number> = signal(0);
   doubleCount: Signal<number> = computed(() => this.count() * 2);
   showCount: WritableSignal<boolean> = signal(false);
   count2: WritableSignal<number> = signal(0);
+
+  // this is to allow effect outside the constructor
+  private injector = inject(Injector);
+
   conditionalCount: Signal<any> = computed(() => {
     if (this.showCount()) {
       return `The count is ${this.count2()}`;
@@ -21,9 +37,15 @@ export class Signals implements OnInit {
     }
   });
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log(`The count is: ${this.count()}`);
+    });
+  }
 
-  ngOnInit = () => void {};
+  ngOnInit() {
+    this.initializeLogging();
+  }
   printCurrentCountValue = (): void => {
     this.count();
   };
@@ -46,5 +68,15 @@ export class Signals implements OnInit {
 
   incrementConditionalCount = (): void => {
     this.count2.update((value) => value + 1);
+  };
+
+  // running effects outside constructor
+  initializeLogging = (): void => {
+    effect(
+      () => {
+        console.log(`Current Count 1 is ${this.count()}`);
+      },
+      { injector: this.injector }
+    );
   };
 }
